74122065f27088a6d77622dbde5951cd480acda2
Joshua Morris <joshua.a.morris@gmail.com>
2013-03-01 03:28:42 -0800

    tree rework

---
+++ skadi/cli/model/ent/dt.py
@@ -0,0 +1,10 @@
+from skadi.cli.model.ent.sendprop import Sendprop
+
+class DT(object):
+    def __init__(self, obj):
+        baseclass = (p.dt_name for p in obj.props if p.var_name == 'baseclass')
+        self.baseclass = next(baseclass, None)
+
+        self.name      = obj.net_table_name
+        self.sendprops = [Sendprop(self, p) for p in obj.props]
+        self.encoded   = obj.needs_decoder
---
+++ skadi/cli/model/ent/tree.py
@@ -0,0 +1,88 @@
+def build_with(dts):
+    tree = Tree(None)
+
+    for dt in dts:
+        lineage = [dt]
+        gen     = (a for a in dts if a.name == dt.baseclass)
+        if dt.baseclass:
+            ancestor = next(gen, None)
+            while ancestor:
+                lineage.insert(0, ancestor)
+                gen      = (a for a in dts if a.name == ancestor.baseclass)
+                ancestor = next(gen, None)
+        [tree.place(dt) for dt in lineage]
+
+    return tree
+
+class NodeFound(Exception):
+    def __init__(self, match):
+        self.match = match
+
+class Node(object):
+    def __init__(self, dt, parent=None):
+        self.dt       = dt
+        self.parent   = parent
+        self.children = []
+
+    def find(self, callable, depth=0):
+        if callable(self.dt):
+            raise NodeFound(self)
+        for child in self.children:
+            child.find(callable, depth=(depth+1))
+
+    def preorder(self, callable, depth=0):
+        callable(depth, self)
+        for child in self.children:
+            child.preorder(callable, depth=depth+1)
+
+class Compares:
+    def __init__(self, dt):
+        self.dt = dt
+
+    def __call__(self, other):
+        return self.dt.name == other.name
+
+class ComparesBaseclass:
+    def __init__(self, dt):
+        self.dt = dt
+
+    def __call__(self, other):
+        return self.dt.baseclass == other.name
+
+class Tree(object):
+    def __init__(self, base_entity):
+        self.root = Node(base_entity)
+
+    def place(self, dt):
+        if self.contains(dt):
+            return
+
+        parent = self._find(ComparesBaseclass(dt))
+        if parent:
+            siblings = parent.children
+            gen      = (s for s in siblings if s.dt.name == dt.name)
+
+            if next(gen, None):
+                return
+
+            node = Node(dt, parent=parent)
+            siblings.append(node)
+        else:
+            node = Node(dt, parent=self.root)
+            self.root.children.append(node)
+
+    def contains(self, dt):
+        return self._find(Compares(dt))
+
+    def preorder(self, callable):
+        for child in self.root.children:
+            child.preorder(callable)
+
+    def _find(self, callable):
+        found = None
+        try:
+            for child in self.root.children:
+                child.find(callable) 
+        except NodeFound as nf:
+            found = nf.match
+        return found
